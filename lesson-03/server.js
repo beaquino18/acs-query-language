0def5374250b56501ab42771629dabfb4f501b01
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2026-09-01 02:49:40 +0800

    completed challenge 1-6, waiting for API to activate

--- lesson-03/server.js
+++ lesson-03/server.js
@@ -3,17 +3,33 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 import 'dotenv/config'
 
 const typeDefs = `#graphql
-  type Test {
-    message: String!
+  enum Units {
+    standard
+    metric
+    imperial
+  }
+
+  type Weather {
+    temperature: Float!
+    description: String!
   }
 
   type Query {
-    test: Test
+    getWeather(zip: Int!, units: Units!): Weather!
   }
 `
 const resolvers = {
   Query: {
-    // resolvers here
+    getWeather: async (_, { zip, units = 'imperial' }) => {
+      const apikey = process.env.OPENWEATHERMAP_API_KEY
+      const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}`
+      const res = await fetch(url)
+      const json = await res.json()
+      console.log(json)
+      const temperature = json.main.temperature
+      const description = json.weather[0].description
+      return { temperature, description }
+    }
   }
 }
 
