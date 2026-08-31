678ac7d73455baad9d7a65894b637e0c28ae2825
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2026-08-31 10:27:53 +0800

    completed challenge 1-6

--- lesson-02/server.js
+++ lesson-02/server.js
@@ -1,38 +1,73 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
+const movieList = [
+  { title: 'Dune: Part Three', genre: 'SciFi', director: 'Denis Villeneuve' },
+  { title: 'Avengers: Doomsday', genre: 'Action', director: 'Anthony and Joe Russo' },
+  { title: 'The Hunger Games: Sunrise on the Reaping', genre: 'SciFi', director: 'Francis Lawrence' },
+  { title: 'Project Hail Mary', genre: 'SciFi', director: 'Phil Lord and Christopher Miller' },
+  { title: '28 Years Later: The Bone Temple', genre: 'Horror', director: 'Nia DaCosta' },
+  { title: 'The Odyssey', genre: 'Adventure', director: 'Christopher Nolan' }
+]
+
+const tvShowList = [
+  { title: 'The Last of Us', genre: 'Horror', seasons: 2 },
+  { title: 'Severance', genre: 'SciFi', seasons: 2 },
+  { title: 'The Pitt', genre: 'Drama', seasons: 2 },
+  { title: 'Abbott Elementary', genre: 'Comedy', seasons: 5}
+]
 
 const typeDefs = `#graphql
-  type About {
-    message: String!
+  enum Genre {
+    SciFi
+    Action
+    Horror
+    Adventure
+    Drama
+    Comedy
   }
 
-  enum MealTime{
-    breakfast
-    lunch
-    dinner
+  interface Media {
+    title: String!
+    genre: Genre!
   }
 
-  type Meal {
-    description: String!
+  type Movie implements Media {
+    title: String!
+    genre: Genre!
+    director: String!
   }
-    
+
+  type TVShow implements Media {
+    title: String!
+    genre: Genre!
+    seasons: Int!
+  }
+  
   type Query {
-    getAbout: About
-    getMeal(time: MealTime!): Meal
+    allMovies: [Movie!]!
+    allTVShows: [TVShow!]!
+    search: [Media!]!
+    getMovieIndex(id: Int!): Movie
   }
 `
 
 // Responsible for returning results of a query
 const resolvers = {
+  Media: {
+    __resolveType(obj) {
+      if (obj.director) return 'Movie'
+      if (obj.seasons) return 'TVShow'
+      return null
+    }
+  },
   Query: {
-    getAbout: () => {
-      return { message: 'Hello World' }
-    },
-    getMeal: (_, { time }) => {
-      const allMeals = { breakfast: 'toast', lunch: 'noodles', dinner: 'pizza'}
-      return { description: allMeals[time] }
-    },
+    allMovies: () => movieList,
+    allTVShows: () => tvShowList,
+    search: () => [...movieList, ...tvShowList],
+    getMovieIndex: (_, { id }) => {
+      return movieList[id]
+    }
   }
 }
 
